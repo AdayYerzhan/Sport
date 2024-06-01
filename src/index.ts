d129f5266e54327a039c59c79ddad54201679b5a
Yerzhan <yerzhan.aday@gmail.com>
2024-06-01 11:33:55 +0500

    Подключаем React и SASS

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import {testFunction} from "./test";
-
-testFunction(5);
-
-document.body.innerHTML = `<div>Hello world</div>`
--- src/test.ts
+++
@@ -1,4 +0,0 @@
-export function testFunction(arg: number): string {
-    console.log('testFunction651651561651651165165')
-    return ''
-}
