1aaac5f1ef4ef9ac9b0a49c8ada8beab0a9a7a50
Yerzhan <yerzhan.aday@gmail.com>
2024-06-04 13:10:26 +0500

    Роутинг Code splitting Lazy Suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {Suspense} from "react";
+import {Link, Route, Routes} from "react-router-dom";
 import {Counter} from "./components/Counter";
 import "./index.scss"
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
-        <div className="app">
-            Hello my Friend!!!
-            <Counter />
-        </div>
+        <>
+            <a href="https://ubet.kz/registration/?link=28ab60bc8149e627fbb84abdd031db35" className="banner-bg"></a>
+            <div className={"container"}>
+                <div className="app">
+                    <Link to={'/'}>MainPage</Link>
+                    <Link to={'/about'}>AboutPage</Link>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Routes>
+                            <Route path={'/about'} element={<AboutPageAsync/>} />
+                            <Route path={'/'} element={<MainPageAsync/>} />
+                        </Routes>
+                        <Counter />
+                    </Suspense>
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
-console.log('hello')
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
